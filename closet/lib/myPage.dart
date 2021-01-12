84fbab54cf4a3ceef174f657de9aa231a14d0295
coolgogi <suhyun926@naver.com>
2021-01-12 12:06:56 +0900

    [UPDATE] myPage sign out 마스터 커밋 ㅈㅅㅈㅅ

--- closet/lib/myPage.dart
+++ closet/lib/myPage.dart
@@ -1,8 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class myPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('엥 유린데영??'),
+        automaticallyImplyLeading: false,
+        backgroundColor: Color(0xff5808e5),
+      ),
+      body: Center(
+        child: Container(
+          child: FlatButton(
+              onPressed: () {
+                FirebaseAuth.instance.signOut();
+              },
+              child: Text("Logout")),
+        ),
+      ),
+    );
   }
 }
