4da14b03b8610cd7fac0be2353cb3ffb506c6bc4
coolgogi <suhyun926@naver.com>
2021-01-14 11:17:30 +0900

    [UPDATE] 연습

---
+++ ML/theme_swicher/lib/theme_provider.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+ThemeData light = ThemeData(
+    brightness: Brightness.light,
+    primarySwatch: Colors.indigo,
+    accentColor: Colors.pink,
+    scaffoldBackgroundColor: Color(0xfff1f1f1));
+
+ThemeData dark = ThemeData(
+    brightness: Brightness.dark,
+    primarySwatch: Colors.indigo,
+    accentColor: Colors.pink,
+    scaffoldBackgroundColor: Color(0xfff1f1f1));
+
+class ThemeNotifier extends ChangeNotifier {
+  final String key = "theme";
+  SharedPreferences _pref;
+  bool _darkTheme;
+  bool get darkTheme => _darkTheme;
+  ThemeNotifier() {
+    _darkTheme = true;
+  }
+  toggleTheme() {
+    _darkTheme = !_darkTheme;
+    notifyListeners();
+  }
+}
