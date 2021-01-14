a4973f25602f77e64680dcd44826cb4a0f947fe8
coolgogi <suhyun926@naver.com>
2021-01-15 02:46:49 +0900

    [UPDATE] mypage

--- closet/lib/myPage.dart
+++ closet/lib/myPage.dart
@@ -1,22 +1,49 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/rendering.dart';
 
-class myPage extends StatelessWidget {
+class myPage extends StatefulWidget {
+  myPage();
+
+  @override
+  _myPage createState() => _myPage();
+}
+
+class _myPage extends State<myPage> {
+  int _currentIndex = 0;
+  List<Widget> _children;
+  bool dark = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('엥 유린데영??'),
+        backgroundColor: Color(0xFF808080),
+        title: Text(
+          "I-Clothes : my Page",
+          style: Theme.of(context).textTheme.headline5,
+        ),
+        actions: <Widget>[
+          IconButton(icon: Icon(Icons.account_circle), onPressed: () {}),
+        ],
         automaticallyImplyLeading: false,
       ),
-      body: Center(
-        child: Container(
-          child: FlatButton(
-              onPressed: () {
-                FirebaseAuth.instance.signOut();
-              },
-              child: Text("Logout")),
-        ),
+      body: Column(
+        children: <Widget>[
+          Container(
+            child: Text(
+              "Dark Mode",
+              textAlign: TextAlign.center,
+            ),
+          ),
+          CupertinoSwitch(
+              value: dark,
+              onChanged: (bool value) {
+                setState(() {
+                  dark = value;
+                });
+              })
+        ],
       ),
     );
   }
