33ddde66694d28af28701ce6635ca42714cd6fd7
coolgogi <suhyun926@naver.com>
2021-01-14 15:19:23 +0900

    [UPDATE] for merge

--- closet/lib/home.dart
+++ closet/lib/home.dart
@@ -86,33 +86,3 @@ class _HomePageState extends State<HomePage> {
     );
   }
 }
-//
-// BottomNavigationBar(
-// type: BottomNavigationBarType.fixed,
-// backgroundColor: Color(0xFF6200EE),
-// selectedItemColor: Colors.white,
-// unselectedItemColor: Colors.white.withOpacity(.60),
-// selectedFontSize: 14,
-// unselectedFontSize: 14,
-// onTap: (value) {
-// // Respond to item press.
-// },
-// items: [
-// BottomNavigationBarItem(
-// title: Text('Favorites'),
-// icon: Icon(Icons.favorite),
-// ),
-// BottomNavigationBarItem(
-// title: Text('Music'),
-// icon: Icon(Icons.music_note),
-// ),
-// BottomNavigationBarItem(
-// title: Text('Places'),
-// icon: Icon(Icons.location_on),
-// ),
-// BottomNavigationBarItem(
-// title: Text('News'),
-// icon: Icon(Icons.library_books),
-// ),
-// ],
-// )
---
+++ closet/lib/theme/darkmode.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+ThemeData light = ThemeData(
+  primaryColorLight: Color(0xff6200EE),
+  secondaryHeaderColor: Color(0xff03DAC6),
+);
